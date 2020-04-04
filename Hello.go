8921861ad75b6d2123dcb10b4e5b36628808304a
Aleksandr <pitforinf@gmail.com>
2020-04-04 23:44:16 +0300

    Рабочий метод Ньютона с небольшими ограничениями.

--- Hello.go
+++ Hello.go
@@ -1,4 +1,4 @@
-package main                            // 1)добавить ввод х и 2)реализовать измененние точности при сравнении
+package main          // не работает, если корень из х больше предустановленного z (сейчас 999999)
 
 import(
   "fmt"
@@ -11,20 +11,19 @@ func Sqrt(x float64) float64 {
 }
 
 func main() {
-  var z, z1, x, n float64 = 11, 0, 18, 1
-//  n := 1
+  var z, z1, x, n float64 = 999999, 0, 18, 1
   i := 0
   fmt.Print("Введите х: ")
   fmt.Scanln(&x)
   fmt.Print("Введите порядок точности для сравнения с функцией math.Sqrt (3..12): ")
   fmt.Scanln(&n)
   s := math.Pow(10, n)
-  fmt.Println(float64(s))
   z0 := Sqrt(x)
-  for ; (z/z0)-1 > (1/s); z = z1 {            // 1/(float64(10^n)))
+  for ; (z/z0)-1 > (1/s); z = z1 {
     z1 = z - (z * z - x) / (2 * z)
     i++
-    fmt.Println(i,") ",z1, "       отношение z/z0 = ", z/z0)
   }
-  fmt.Println("    ",z0, " - Квадратный корень math.Sqrt(x)")
+  fmt.Println("Выполнено ", i," итерации(й) вычисления.")
+  fmt.Println(z1, " -- значение, полученное методом Ньютона")
+  fmt.Println(z0, " -- Квадратный корень, вычесленный функцией math.Sqrt(x)")
 }
