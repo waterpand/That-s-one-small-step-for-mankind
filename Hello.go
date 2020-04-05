eb29575617c3b540868f6e5834b70c2e24be81e8
Aleksandr <pitforinf@gmail.com>
2020-04-05 22:52:18 +0300

    Нахождение сумм четных и нечетных цифр числа

--- Hello.go
+++ Hello.go
@@ -1,19 +1,19 @@
-package main		// Найти сумму нечетных цифр числа          (a % b) - остаток от деления
+package main		// Найти сумму четных и нечетных цифр числа          (a % b) - остаток от деления
 
 import "fmt"
-var n, d, sum int
+var n, d, sum1, sum2 int
 func main(){
-	fmt.Println("Нахождение суммы нечетных цифр числа.")
+	fmt.Println("Нахождение сумм четных и нечетных цифр числа.")
 	fmt.Print("Введите целое число:")
 	fmt.Scanln(&n)
 	for ; n > 0 ; n = n/10 {
 		d = (n % 10)
 		if ((d % 2) != 0) {
-			sum = sum + d
+			sum1 = sum1 + d
+		} else {
+			sum2 = sum2 + d
 		}
-		// fmt.Println("Число =", n)
-		// fmt.Println("Остаток =", d)
-		// fmt.Println("Сумма =", sum)
 	}
-	fmt.Println("Сумма =", sum)
+	fmt.Println("Сумма четных цифр числа:", sum2)
+	fmt.Println("Сумма нечетных цифр числа:", sum1)
 }
