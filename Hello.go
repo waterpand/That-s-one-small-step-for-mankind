cac060a09ac4a11565926d8a9ceebad6c797cc7d
Aleksandr <pitforinf@gmail.com>
2020-04-05 16:23:36 +0300

    defer

--- Hello.go
+++ Hello.go
@@ -5,22 +5,11 @@ import(
   )
 
 func main() {
-    var i int = 0
-    fmt.Print("Введи число 1..5: ")
-    fmt.Scanln(&i)
-	fmt.Print("write ", i, " as ")
-	switch i {
-	case 1:
-		fmt.Println("one")
-	case 2:
-		fmt.Println("two")
-	case 3:
-		fmt.Println("three")
-    case 4:
-        fmt.Println("foure")
-    case 5:
-        fmt.Println("five")
-	default:
-		fmt.Println("wrong number!")
-	}
+    fmt.Println("counting")
+    defer fmt.Println("done")
+
+    for i := 0; i < 11; i++ {
+        defer fmt.Println(i)
+    }
+    defer fmt.Println("Ok")
 }
