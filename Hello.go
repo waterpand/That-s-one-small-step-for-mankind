4b007ae6cfb786b7ae134a80cc755d1eede57c88
Aleksandr <pitforinf@gmail.com>
2020-04-05 21:01:39 +0300

    массивы и срезы

--- Hello.go
+++ Hello.go
@@ -1,15 +1,26 @@
+                                                // a[0:10] эти записи эквивалентны
+                                                // a[:10] Значением по умолчанию для нижней границы является нуль, а для верхней - размер среза.
+                                                // a[0:]
+                                                // a[:]
+
+
+
 package main
 
-import(
-  "fmt"
-  )
+import "fmt"
 
 func main() {
-    fmt.Println("counting")
-    defer fmt.Println("done")
+	s := []int{2, 3, 5, 7, 11, 13}
 
-    for i := 0; i < 11; i++ {
-        defer fmt.Println(i)
-    }
-    defer fmt.Println("Ok")
+	s = s[1:4]                                 // [3 5 7]
+	fmt.Println(s)
+
+	s = s[:2]                                  // [3 5]
+	fmt.Println(s)
+
+	s = s[1:]                                  // [5]
+	fmt.Println(s)
+
+    fmt.Println(3*3)
+    fmt.Println(1E3)
 }
