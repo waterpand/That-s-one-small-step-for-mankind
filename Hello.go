e96ed1c5d2c2d0d388c77821e891ab7d650c4ee9
Aleksandr <pitforinf@gmail.com>
2020-04-07 20:50:59 +0300

    Определить из каких цифр состоит число

--- Hello.go
+++ Hello.go
@@ -1,27 +1,23 @@
-package main //Задача 2. Найти сумму и количество элементов последовательности, которые по модулю больше 0.001.
-//S = 1/2 - 2/4 + 3/8 - 4/16 + ... - ...
+package main //Задача. Определить из каких цифр состоит число
+
 //(a % b) - остаток от деления
 
 import (
 	"fmt"
-	"math"
 )
 
-var i int = 1
-var a, b, sum, z float64 = 1, 2, 0, 1
+var a, b int
 
 func main() {
-	for math.Abs(float64(a/b)) > 0.001 {
-		sum = sum + (a/b)*z
-		z = -z
-		fmt.Print("step", i, "   ")
-		fmt.Print("a=", a)
-		fmt.Print("   b=", b)
-		fmt.Println("    sum= ", sum)
-		i++
-		a = (math.Abs(a) + 1)
-		b = b * 2
+	fmt.Print("введите число")
+	fmt.Scanln(&a)
+	for ; a > (99 / 100); a = a / 10 {
+		b = b*10 + (a % 10)
 
 	}
-	fmt.Println("Сумма первых", i, "элементов последовательности равна", sum)
+
+	for ; b > (99 / 100); b = b / 10 {
+		fmt.Print((b % 10), ",")
+	}
+
 }
