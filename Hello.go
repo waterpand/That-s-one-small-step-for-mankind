f3a52a5ebe0485e4b27fecca8667e9cf2e8a4f2c
Aleksandr <pitforinf@gmail.com>
2020-04-12 20:00:21 +0300

    Последовательности чисел, упорядоченные по возрастанию

--- Hello.go
+++ Hello.go
@@ -1,4 +1,4 @@
-package main //Задача. В одномерном массиве удалить все четные элементы и оставить только нечетные.
+package main //Задача. Найти самые длинные последовательности чисел, упорядоченные по возрастанию.
 
 import (
 	"fmt"
@@ -7,28 +7,38 @@ import (
 )
 
 var (
-	m    [25]int
-	s    []int
+	m    [50]int
+	s, t []int
 	x, y int
 )
 
-func arrCr(x, y int) [25]int {
+func arrCr(x, y int) [50]int {
 	rand.Seed(time.Now().UnixNano())
-	for i := 0; i < 25; i++ {
+	for i := 0; i < 50; i++ {
 		m[i] = rand.Intn(x) - y
 	}
 	return m
 }
 
 func main() {
-	x = 99
-	y = 50
+	x = 16
+	y = 0
 	m = arrCr(x, y)
+	fmt.Println(m)
 	for i := 0; i < len(m); i++ {
-		if m[i]%2 != 0 {
-			s = append(s, m[i])
+		s := m[i:] // срез от текущей позиции и до конца массива
+		//fmt.Println(s)
+		for j := 0; j < len(s)-1; j++ {
+			if s[j] > s[j+1] {
+				if j >= 1 {
+					fmt.Println(s[:j+1])
+					i = i + j  // первый цикл перепрыгнет отработанный срез
+					j = len(s) // выход из второго цикла
+				} else {
+					i = i + j // происходит тоже самое, что и в случае с true, но срез не выводится на печать, если он состоит из одного элемента
+					j = len(s)
+				}
+			}
 		}
 	}
-	fmt.Println(m)
-	fmt.Println(s)
 }
