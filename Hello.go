653177d718b68754463ad42db184b40866c008d6
Aleksandr <pitforinf@gmail.com>
2020-04-13 20:26:30 +0300

    Многомерный срез и оператор range

--- Hello.go
+++ Hello.go
@@ -8,10 +8,10 @@ import (
 )
 
 var (
-	m                     [80]int
-	s                     []int
-	t, w, q               [][]int
-	x, y, z, max, k, l, c int
+	m                  [80]int
+	s                  []int
+	t, w, q            [][]int
+	x, y, z, max, k, c int
 )
 
 func arrCr(x, y int) [80]int {
@@ -26,13 +26,14 @@ func main() {
 	x = 16
 	y = 0
 	m = arrCr(x, y)
+	fmt.Println("Исходный массив: ")
 	fmt.Println(m)
-	for i := 0; i < len(m); i++ {
+	for i := range m { // вместо for i := 0; i < len(m); i++ {
 		s := m[i:] // срез от текущей позиции и до конца массива
 		for j := 0; j < len(s)-1; j++ {
 			if s[j] > s[j+1] {
 				if j >= 1 {
-					fmt.Println(s[:j+1])
+					//fmt.Println(s[:j+1])
 					t = append(t, s[:j+1])
 					i = i + j  // первый цикл перепрыгнет отработанный срез
 					j = len(s) // выход из второго цикла
@@ -43,25 +44,27 @@ func main() {
 			}
 		}
 	}
-	fmt.Println("срез срезов: ", t)
-	fmt.Println(len(t))
+	fmt.Println("Все увеличивающиеся последовательности чисел: ")
+	fmt.Println(t)
+	fmt.Println("Количество этих последовательностей: ", len(t))
 	c = len(t) - 1
-	for l := 0; l < c; l++ {
+	for i := 0; i < c; i++ { //for i := range t {  -- здесь не подходит, не совсем понимаю как, но он вызывает ошибку на полденей итерации.
 		k = len(t)
 		max = 0
-		for i := 0; i < len(t)-1; i++ { // попробовать использовать range  https://go-tour-ru-ru.appspot.com/moretypes/16
-			if len(t[i]) > max && i != k {
-				max = len(t[i])
-				k = i
+		for l := 0; l < len(t)-1; l++ { // попробовать использовать range  https://go-tour-ru-ru.appspot.com/moretypes/16
+			if len(t[l]) > max && l != k {
+				max = len(t[l])
+				k = l
 			}
 		}
-		fmt.Println(t[k])
+		//fmt.Println(t[k])
 		w = append(w, t[k])
 		t = append(t[:k], t[k+1:]...)
-		fmt.Println("Срез w: ", w)
-		fmt.Println("Срез t: ", t)
+		//fmt.Println("Срез w: ", w)
+		//fmt.Println("Срез t: ", t)
 	}
 	w = append(w, t...)
-	fmt.Println("Полный Срез w: ", w)
-	fmt.Println("Пустой Срез t: ", t)
+	fmt.Println("Увеличивающиеся последовательности чисел отранжированные по длине: ")
+	fmt.Println(w)
+	//fmt.Println("Пустой Срез t: ", t)
 }
