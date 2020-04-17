0eb8119b987d8423ab20ccc40499b8e91e4d829e
Aleksandr <pitforinf@gmail.com>
2020-04-17 20:56:44 +0300

    При изменении tempList, priceList остается неизменным

--- Hello.go
+++ Hello.go
@@ -24,6 +24,7 @@ var (
 		11: {"Укроп", 23}, 12: {"Петрушка", 18}, 13: {"Масло", 178.40}, 14: {"Молоко", 67}, 15: {"Сахар", 54},
 		16: {"Чай", 80}, 17: {"Виноград", 145},
 	}
+	tempList, t map[int]Goods
 	weight, sum float64
 	j           int = 20
 )
@@ -38,13 +39,17 @@ func listP(tempList map[int]Goods, d int) {
 	}
 }
 
+func createMap(h map[int]Goods) map[int]Goods {
+	t = make(map[int]Goods)
+	for k, v := range h {
+		t[k] = v
+	}
+	return t
+}
+
 func main() {
-	tempList := priceList
 	d := 0
-	// fmt.Println("Всегда в наличии ", len(tempList), "наименований товара: ")
-	// for i := 1; i < 18; i++ {
-	// 	fmt.Println(i, "--", tempList[i])
-	//}
+	tempList = createMap(priceList)
 	for j != 0 {
 		listP(tempList, d)
 		fmt.Print("Введите номер товара или 0 для завершения покупок: ")
@@ -53,25 +58,25 @@ func main() {
 		if j != 0 && j <= len(tempList)+d && ok == true {
 			//p := tempList[j]
 			fmt.Print("Вы выбрали ", p.title, " Введите необходимый вес или количество: ")
-			fmt.Println(priceList[j])
 			fmt.Scanln(&weight)
 			sum = sum + weight*p.price
 			delete(tempList, j)
 			d = d + 1 // Не знаю как сделать иначе// счетчик удалений, компенсирует уменьшение длины массива, из-за чего список выводится не весь.
 		} else if j > len(tempList)+d && j != 99 {
 			fmt.Println("Введен неверный номер товара! Попробуйте снова")
-		} else if j == 99 {
-			for q := 1; q <= len(priceList); q++ { //После каждого удаления элемента в функцию возвращается карта с меньшим len и список выводится не весь
+		} else if j == 99 { // этот цикл для проверки первоначальной карты
+			for q := 1; q <= len(priceList); q++ {
 				om, okk := priceList[q]
 				if okk == true {
-					fmt.Println(q, "--", om)
+					fmt.Println(q, "-+-", om)
 				} else {
 					fmt.Println("Такого ключа не существует")
 				}
 			}
 
 		} else if j != 0 && j < len(tempList)+d {
-			fmt.Println("Вы уже взяли этот товар")
+			f := priceList[j]
+			fmt.Println("Вы уже взяли", f.title, "выберите другой товар или 0 для завершения")
 		}
 	}
 	fmt.Println("Всего покупок на ", sum, "руб.")
