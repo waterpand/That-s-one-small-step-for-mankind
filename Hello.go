974feab7c946c3c475884b81ad7d04af4324893f
Aleksandr <pitforinf@gmail.com>
2020-04-18 15:15:14 +0300

    Добавляет в конец и в середину списка

--- Hello.go
+++ Hello.go
@@ -5,7 +5,7 @@ package main //Задача. Определить сумму покупки.
 //Как узнать размер map? --> len(priceList) <--
 //Предусмотреть ошибочный ввод номера товара  --> elem, ok = m[key] <--
 // Группы товаров..
-// сделать обновление листа для добавленных или удаленных
+// сделать обновление листа для добавленных или удаленных -- не работает, проблема в размере карты
 
 import (
 	"fmt"
@@ -48,9 +48,29 @@ func createMap(h map[int]Goods) map[int]Goods {
 	return t
 }
 
-func addGoods(tit string, pr float64) { // чтобы не просто добавлять в конец, а заполнять пустоты нужно сделать через range...наверное
-	key := len(priceList) + 1
-	priceList[key] = Goods{tit, pr}
+func addGoods(tit string, pr float64, z int) int {
+	stop := true
+	fmt.Println("1", stop, z)
+	for i := 1; i <= z; i++ {
+		_, ok := priceList[i]
+		fmt.Println("2", stop, ok)
+		if ok != true && stop == true {
+			priceList[i] = Goods{tit, pr}
+			stop = false
+			fmt.Println("3", stop, i, z)
+		}
+	}
+	fmt.Println("4", stop, z)
+	if stop == true {
+		key := z + 1
+		z = z + 1
+		fmt.Println("5", stop, key, z)
+		fmt.Println(key)
+		priceList[key] = Goods{tit, pr}
+		fmt.Println(priceList[key])
+
+	}
+	return z
 }
 
 func delGoods(x int) {
@@ -58,6 +78,7 @@ func delGoods(x int) {
 }
 
 func main() {
+	z := len(priceList)
 	d := 0
 	tempList = createMap(priceList)
 	for j != 0 {
@@ -72,29 +93,32 @@ func main() {
 			sum = sum + weight*p.price
 			delete(tempList, j)
 			d = d + 1 // Не знаю как сделать иначе// счетчик удалений, компенсирует уменьшение длины массива, из-за чего список выводится не весь.
-		} else if j > len(tempList)+d && j != 99 && j != 101 && j != 202 {
+		} else if j > len(tempList)+d && j != 99 && j != 101 && j != 202 && j != 303 {
 			fmt.Println("Введен неверный номер товара! Попробуйте снова")
 		} else if j == 99 { // этот цикл для проверки первоначальной карты
-			for q := 1; q <= len(priceList); q++ {
+			fmt.Println(d, "d")
+			for q := 1; q <= z; q++ {
 				om, okk := priceList[q]
 				if okk == true {
 					fmt.Println(q, "-+-", om)
 				} else {
-					fmt.Println("Такого ключа не существует")
+					fmt.Println(q, "Данный товар был удален")
 				}
 			}
-
 		} else if j == 101 { // добавление элемента
 			fmt.Println("Добавление товара в список: ")
 			fmt.Print("Введите название товара ")
 			fmt.Scanln(&tit)
 			fmt.Print("Введите цену на ", tit, " ")
 			fmt.Scanln(&pr)
-			addGoods(tit, pr)
+			z = addGoods(tit, pr, z)
 		} else if j == 202 { // удаление товара
 			fmt.Print("Выберите товар, который будет удален из списка: ")
 			fmt.Scanln(&x)
 			delGoods(x)
+			d = d + 1
+		} else if j == 303 {
+			tempList = createMap(priceList)
 		} else if j != 0 && j <= len(tempList)+d {
 			f := priceList[j]
 			fmt.Println("Вы уже взяли", f.title, "выберите другой товар или 0 для завершения")
