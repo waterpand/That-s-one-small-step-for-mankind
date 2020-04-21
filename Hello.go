bfb32a757413f44eae641ffad67cdd9daec6c498
Aleksandr <pitforinf@gmail.com>
2020-04-21 15:49:16 +0300

    Определить количество элементов массива больше соседних элементов

--- Hello.go
+++ Hello.go
@@ -1,4 +1,4 @@
-package main //Задача. Вывести на экран значения массива по спирали. Будем понимать под этим следующее. Если массив состоит из 100 элементов, то выводить по 10 элементов в каждой строке, при этом каждая вторая строка должна выводиться "задом наперед": сначала последние элементы строки, а затем первые.
+package main //Задача. Определить количество элементов массива, значение которых больше соседних элементов
 import (
 	"fmt"
 	"math/rand"
@@ -6,8 +6,8 @@ import (
 )
 
 var (
-	m       []int
-	z, y, x int
+	m          []int
+	z, y, x, q int
 )
 
 func slCr(x, y, z int) []int {
@@ -20,41 +20,17 @@ func slCr(x, y, z int) []int {
 }
 
 func main() {
-	x = 1000
+	x = 100
 	y = 0
 	fmt.Println("Введите размер массива: ")
 	fmt.Scan(&z)
 	m = slCr(x, y, z)
-	for i := 0; i < z; i++ {
-		if (i/10)%2 != 0 && i%10 == 9 {
-			fmt.Println("   ", m[i])
-		} else if (i/10)%2 != 0 {
-			fmt.Print("   ", m[i])
-		} else if (i/10)%2 == 0 && i%10 == 9 {
-			fmt.Println("   ", m[i])
-		} else if (i/10)%2 == 0 {
-			fmt.Print("   ", m[i])
+	for i := 1; i < z-1; i++ {
+		if m[i] > m[i-1] && m[i] > m[i+1] {
+			fmt.Print("   ", i, ") - ", m[i])
+			q = q + 1
 		}
 	}
 	fmt.Println()
-	for i := 0; i < z; i = i + 10 {
-		switch (i/10)%2 == 0 {
-		case true:
-			fmt.Print("---> ")
-			for k := i; k < i+10; k++ {
-				if k < len(m) {
-					fmt.Print(m[k], " ")
-				}
-			}
-			fmt.Println()
-		case false:
-			fmt.Print("<--- ")
-			for k := i + 9; k >= i; k-- {
-				if k < len(m) {
-					fmt.Print(m[k], " ")
-				}
-			}
-			fmt.Println()
-		}
-	}
+	fmt.Println(q)
 }
